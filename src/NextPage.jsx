bbc3910e165723416685604e2957c0acce02f7b9
nauman <naumanshoail37@gmail.com>
2024-09-24 05:55:09 -0700

    code updated

--- src/NextPage.jsx
+++ src/NextPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
-
+import {motion} from "framer-motion"
 
 function NextPage() {
 const [Counter , setCounter] = useState(0)
@@ -28,7 +28,32 @@ useEffect(()=>{console.log('The count is :' , Counter  )} , [Counter])
       <button onClick={increase}>INCREAMENT</button>
       <br />
       <button onClick={decrease}>DECREMENT</button>
+
+ <motion.div 
+ className='box1 h-[150px] w-[200px] m-10 bg-blue-300'
+style={{
+
+    opacity:0.2
+}}
+
+ animate = {{
+    x:750,
+    opacity:3
+ }}
+
+
+>
+
+ </motion.div>
+
+
+
+
     </div>
+
+
+
+
   );
 }
 
